01532e1a276394e1f4f65857633fd555f1eee307
scrungus <tyler@stackhpc.com>
2024-06-28 12:32:19 +0100

    database connection

--- coral_credits/settings.py
+++ coral_credits/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = "coral_credits.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": "/data/db.sqlite3",
     }
 }
 
