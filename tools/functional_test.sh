87afa69d38eca69027533c4f7f3e66e4ce80a5d2
scrungus <tyler@stackhpc.com>
2024-07-01 21:26:59 +0100

    add wait + status check

--- tools/functional_test.sh
+++ tools/functional_test.sh
@@ -13,5 +13,23 @@ helm upgrade coral-credits ./charts \
   --install \
   --wait \
   --timeout 10m \
-  --set-string image.tag=${GITHUB_SHA::7} \
+  --set-string image.tag=${GITHUB_SHA::7}
 
+# Wait for deploy to become ready
+kubectl wait --for=condition=available deployment -n coral-credits \
+-l "app.kubernetes.io/name=coral-credits,app.kubernetes.io/instance=coral-credits" \
+--timeout=300s
+
+# Port forward in the background
+kubectl port-forward -n coral-credits svc/coral-credits 8080:8080 &
+
+# Check we get a 204 from the status endpoint
+if [ "$(curl -s -o /dev/null -w "%{http_code}" http://0.0.0.0:8080/_status/)" -eq 204 ]; then
+    echo "Success: HTTP status code is 204."
+    exit 0
+else
+    echo "Error: Expected HTTP status code 204, but got $(curl -s -o /dev/null -w "%{http_code}" http://0.0.0.0:8080/_status/)"
+    exit 1
+fi
+
+#TODO(tylerchristie) check more things
