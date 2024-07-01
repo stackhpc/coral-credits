446e9a6fc6d26c0913cc8b9f3861232b2ea6f001
scrungus <tyler@stackhpc.com>
2024-07-01 20:53:09 +0100

    turn debug off in prod, allowed hosts

--- coral_credits/settings.py
+++ coral_credits/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure--%%s+1^nkk97qur-52c0tbi4g(wxgtwuaau14etjrhvhqo0+i#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
