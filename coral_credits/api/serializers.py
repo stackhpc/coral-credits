242e23bdec184e8ac7fd33b32b7120cfa1bb20b9
scrungus <tyler@stackhpc.com>
2024-07-05 14:55:26 +0100

    ConsumerRequest method adapted for blazar requests

--- coral_credits/api/serializers.py
+++ coral_credits/api/serializers.py
@@ -62,8 +62,56 @@ class Consumer(serializers.ModelSerializer):
         fields = ["consumer_ref", "resource_provider", "start", "end", "resources"]
 
 
+class ContextSerializer(serializers.Serializer):
+    user_id = serializers.UUIDField()
+    project_id = serializers.UUIDField()
+    auth_url = serializers.URLField()
+    region_name = serializers.CharField()
+
+class ResourceRequestSerializer(serializers.Serializer):
+    # simple case ; we don't distinguish between types of cpu
+    # TODO(tylerchristie) change this
+    cpu = serializers.CharField(help_text="Either 'vcpu' or 'pcpu'")
+    memory = serializers.IntegerField(help_text="Memory in MB")
+    storage = serializers.IntegerField(help_text="Storage in GB")
+    storage_type = serializers.CharField()
+
+class AllocationSerializer(serializers.Serializer):
+    id = serializers.CharField()
+    hypervisor_hostname = serializers.UUIDField()
+    extra = serializers.DictField()
+
+class ReservationSerializer(serializers.Serializer):
+    resource_type = serializers.CharField()
+    min = serializers.IntegerField()
+    max = serializers.IntegerField()
+    hypervisor_properties = serializers.CharField()
+    resource_properties = serializers.CharField()
+    allocations = serializers.ListField(child=AllocationSerializer(), required=False, allow_null=True)
+    resource_requests = ResourceRequestSerializer() # TODO item
+
+class LeaseSerializer(serializers.Serializer):
+    lease_id = serializers.UUIDField()  # TODO item
+    start_date = serializers.DateTimeField()
+    end_time = serializers.DateTimeField()
+    reservations = serializers.ListField(child=ReservationSerializer())
+
 class ConsumerRequest(serializers.Serializer):
-    consumer_ref = serializers.CharField(max_length=200)
-    resource_provider_id = serializers.IntegerField()
-    start = serializers.DateTimeField()
-    end = serializers.DateTimeField()
+    def __init__(self, *args, current_lease_required=False, **kwargs):
+        super().__init__(*args, **kwargs)
+        # current_lease required on update but not create
+        self.fields['current_lease'] = LeaseSerializer(
+            required=current_lease_required, 
+            allow_null=(not current_lease_required)
+        )
+
+    context = ContextSerializer()
+    lease = LeaseSerializer()
+
+    def to_internal_value(self, data):
+        # Custom validation or processing can be added here if needed
+        return super().to_internal_value(data)
+
+    def to_representation(self, instance):
+        # Custom representation logic can be added here if needed
+        return super().to_representation(instance)
