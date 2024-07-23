57bfc7afe8541e2ca4d5d2798f52a7eb68054961
scrungus <tyler@stackhpc.com>
2024-07-23 18:34:34 +0100

    urls changed

--- coral_credits/urls.py
+++ coral_credits/urls.py
@@ -25,12 +25,12 @@ from coral_credits.api import views
 router = routers.DefaultRouter()
 router.register(r"resource_class", views.ResourceClassViewSet)
 router.register(r"resource_provider", views.ResourceProviderViewSet)
-router.register(r"allocations", views.CreditAllocationViewSet)
+router.register(r"allocation", views.CreditAllocationViewSet)
 router.register(r"account", views.AccountViewSet, basename="creditaccount")
 router.register(r"consumer", views.ConsumerViewSet, basename="resource-request")
 
 allocation_router = routers.NestedSimpleRouter(
-    router, r"allocations", lookup="allocation"
+    router, r"allocation", lookup="allocation"
 )
 allocation_router.register(
     r"resources", views.CreditAllocationResourceViewSet, basename="allocation-resource"
@@ -51,5 +51,3 @@ urlpatterns = [
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("admin/", admin.site.urls),
 ]
-
-urlpatterns += router.urls
